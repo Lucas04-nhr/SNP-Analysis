0cdeebea252c024f4d8b65149bc2067f8ad39404
Lucas04-nhr <lucas04@foxmail.com>
2024-10-25 22:03:11 +0800

    Refactor create_phenotype_and_covariant.py script to separate phenotype and covariant data and save them in plink folder

---
+++ script/create_phenotype_and_covariant.py
@@ -0,0 +1,40 @@
+import numpy
+import pandas
+
+# Test IDs
+test_ids = ['BJ001', 'BJ002', 'BJ004', 'BJ005', 'BJ006', 'BJ008']
+
+# Load original data
+data_covariate = pandas.read_csv('data/export/manifestotype.csv')
+print(data_covariate.head())
+
+data_covariate_test = data_covariate[data_covariate['Sample id'].isin(test_ids)]
+print(data_covariate_test)
+
+data_phenotype = pandas.read_csv('data/export/abundance_bj.csv')
+print(data_phenotype.head())
+
+# Check the column names to find the correct one
+print(data_phenotype.columns)
+
+# Assuming the correct column name is 'Sample id'
+data_phenotype_test = data_phenotype[data_phenotype['Genus'].isin(test_ids)]
+print(data_phenotype_test)
+
+# Create phenotype data frame
+phenotype = data_phenotype_test.rename(columns={'Genus': 'Family ID'})
+phenotype['Individual ID'] = phenotype['Family ID']
+phenotype = phenotype[['Family ID', 'Individual ID'] + [col for col in phenotype.columns if col not in ['Family ID', 'Individual ID']]]
+print(phenotype.head())
+
+# Save phenotype data to file
+phenotype.to_csv('data/plink/phenotype.tsv', sep='\t', index=False)
+
+# Create covariant data frame
+covariant = data_covariate_test.rename(columns={'Sample id': 'Family ID'})
+covariant['Individual ID'] = covariant['Family ID']
+covariant = covariant[['Family ID', 'Individual ID'] + [col for col in covariant.columns if col not in ['Family ID', 'Individual ID']]]
+print(covariant.head())
+
+# Save covariate data to file
+covariant.to_csv('data/plink/covariate.tsv', sep='\t', index=False)
