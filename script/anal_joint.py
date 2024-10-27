0dd17e39248a22d500b32d77068a62ca273f0d3f
Lucas04-nhr <lucas04@foxmail.com>
2024-10-27 18:21:18 +0800

    Refactor anal_joint.py script to add plot_mqq() function

--- script/anal_joint.py
+++ script/anal_joint.py
@@ -4,4 +4,15 @@ import pandas as pd
 
 # Load data
 data = pd.read_table('data/plink/result.P5.assoc.linear', sep='\s+')
-print(data.head())
+
+# Draw the plot
+stats = gl.Sumstats(
+  data,
+  snpid="SNP",
+  chrom="CHR",
+  pos="BP",
+  p="P",
+  verbose=False
+  )
+
+stats.plot_mqq()
