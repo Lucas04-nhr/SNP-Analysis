9f4000e6cd5e7a8fa6f44009352f89f9af685721
Lucas04-nhr <lucas04@foxmail.com>
2024-11-29 21:57:09 +0800

    Enhance combine_bacteria.py: update file paths and change column mapping from 'Bacteria' to 'Cov'

--- script/combine_bacteria.py
+++ script/combine_bacteria.py
@@ -2,15 +2,15 @@ import pandas as pd
 import numpy as np
 
 # Load the data
-result = pd.read_csv('result/analysis/merged_c004_GZ.csv')
-bacteria_names = pd.read_csv('result/analysis/bacteria_GZ.csv')
+result = pd.read_csv('result/analysis/snp_allocation/cov-as-phe_numeric_age_GZ_ori.csv')
+bacteria_names = pd.read_csv('result/analysis/top_bacteria/cov_as_phe/phenotype_numeric_GZ.csv')
 
 # Change the content
 # Create a dictionary from the bacteria DataFrame for mapping
 bacteria_dict = pd.Series(bacteria_names.iloc[:, 1].values, index=bacteria_names.iloc[:, 0]).to_dict()
 
-# Replace the values in the 'bacteria' column of the result DataFrame
-result['Bacteria'] = result['Bacteria'].map(bacteria_dict)
+# Replace the values in the 'Cov' column of the result DataFrame
+result['Cov'] = result['Cov'].map(bacteria_dict)
 
 # Save the result
-result.to_csv('result/analysis/04_age_GZ.csv', index=False)
+result.to_csv('result/analysis/snp_allocation/cov-as-phe_numeric_age_GZ_rel.csv', index=False)
