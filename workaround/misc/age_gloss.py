3260061ce063035aab32432c6dc62f2bf2b64d20
Lucas <lucas04@foxmail.com>
2025-03-23 22:29:46 +0800

    feat: Add age-gloss analysis and visualization for Beijing and Guangzhou samples; update moisture data type to float

--- workaround/misc/age_gloss.py
+++ workaround/misc/age_gloss.py
@@ -24,18 +24,18 @@ manifestotype_gz = manifestotype_gz.sort_values(by='Age')
 manifestotype_all = manifestotype_all.sort_values(by='Age')
 
 # Change the data type of moisture to float
-manifestotype_bj['Moisture'] = pd.to_numeric(manifestotype_bj['Moisture'], errors='coerce')
-manifestotype_gz['Moisture'] = pd.to_numeric(manifestotype_gz['Moisture'], errors='coerce')
-manifestotype_all['Moisture'] = pd.to_numeric(manifestotype_all['Moisture'], errors='coerce')
+manifestotype_bj['Gloss'] = pd.to_numeric(manifestotype_bj['Moisture'], errors='coerce')
+manifestotype_gz['Gloss'] = pd.to_numeric(manifestotype_gz['Moisture'], errors='coerce')
+manifestotype_all['Gloss'] = pd.to_numeric(manifestotype_all['Moisture'], errors='coerce')
 
 
 # Dot plot of Beijing Age-Moisture relations
 plt.figure(figsize=(12, 8), dpi=100)
-plt.scatter(manifestotype_bj['Age'], manifestotype_bj['Moisture'], c='red', alpha=0.5, s=20)
-plt.scatter(manifestotype_gz['Age'], manifestotype_gz['Moisture'], c='blue', alpha=0.5, s=20)
-plt.title('Age-Moisture Relations of Samples in Beijing and Guangzhou')
+plt.scatter(manifestotype_bj['Age'], manifestotype_bj['Gloss'], c='red', alpha=0.5, s=20)
+plt.scatter(manifestotype_gz['Age'], manifestotype_gz['Gloss'], c='blue', alpha=0.5, s=20)
+plt.title('Age-Gloss Relations of Samples in Beijing and Guangzhou')
 plt.xlabel('Age')
-plt.ylabel('Moisture')
+plt.ylabel('Gloss')
 ages = sorted(manifestotype_all['Age'].unique())
 moistures_bins = np.arange(20, 101, 10)
 plt.gca().set_xticks(ages[::5]) # Set x-axis ticks to show every 5th age value
@@ -44,5 +44,5 @@ plt.gca().set_yticks(moistures_bins) # Set y-axis ticks to moisture bin
 
 plt.legend(['Beijing', 'Guangzhou'], loc='upper left')
 
-plt.savefig('result/essay/age_moisture.pdf')
+plt.savefig('result/essay/age_gloss.pdf')
 # plt.show()
