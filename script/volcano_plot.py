3598af9b902ffee66dd74ea74f646a84ee7c5319
Lucas <lucas04@foxmail.com>
2025-06-11 13:42:36 +0800

    feat: Implement initial volcano plot script for SNP analysis

---
+++ script/volcano_plot.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+plink_filtered_file = '/mnt/raid6/bacphagenetwork/data/12_plink/Full/merged/filtered_bac_age.csv'
+
+df = pd.read_csv(plink_filtered_file)
